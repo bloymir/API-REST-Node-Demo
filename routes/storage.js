210f482406cf3572aae1b83f72778292c1059419
Nelson Tapia <ntapiapinto@gmail.com>
2022-07-20 20:13:13 -0400

    Se abstrae handleStorage

--- routes/storage.js
+++ routes/storage.js
@@ -1,26 +1,10 @@
-const multer = require('multer')
+
 const express = require('express');
 const router = express.Router();
+const uploadMiddleware = require('../utils/handleStorage')
 
-/**
- * 
- */
-const storage = multer.diskStorage({
-    destination: function(req, file, cb){
-        const pathStorage = `${__dirname}/../storage`;
-        cb(null, pathStorage);
-    },
-    filename: function(req, file, cb){
-        const ext = file.originalname.split(".").pop();
-        const filename = `file-${Date.now()}.${ext}`;
-        cb(null, filename);
-    }
-})
-/**
- * 
- */
 
-router.post("/", (req, res) => {
+router.post("/", uploadMiddleware.single("myfile"), (req, res) => {
     res.send({a:1})
 })
 
